42499a202f705bcee7fe287c691e1a960d0fe6c6
leocelbun <leorobig1@gmail.com>
2024-05-30 12:11:59 +0100

    Update fitonai_contract.sol

--- fitonai_contract.sol
+++ fitonai_contract.sol
@@ -1,1 +1,25 @@
+pragma solidity ^0.8.0;
+
+contract FitonAiInvestmentFund {
+    address public manager;
+    mapping(address => uint) public investors;
+    
+    constructor() {
+        manager = msg.sender;
+    }
+    
+    function invest() public payable {
+        require(msg.value > 0.01 ether);
+        investors[msg.sender] += msg.value;
+    }
+    
+    function getBalance() public view returns(uint) {
+        return address(this).balance;
+    }
+    
+    function withdraw(uint amount) public {
+        require(msg.sender == manager);
+        payable(manager).transfer(amount);
+    }
+}
 
